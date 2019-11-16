8f4be98d6eced3fab80e2785a21b290d891119fe
shilf <tcl.shilf@gmail.com>
2019-11-16 17:50:02 +0800

    amakeroot重新设置一下

--- script/Amake/build_spot/amake/amake.bat
+++ script/Amake/build_spot/amake/amake.bat
@@ -1,3 +1,3 @@
 @echo off
-set amakeroot=C:\Tools\cmder\plugin\script\Amake\build\amake
+set amakeroot=C:\Tools\cmder\plugin\script\Amake\build_spot\amake
 nmake /NOLOGO -f %amakeroot%\windows\makefile.win prjfile=%1 amakeroot=%amakeroot%
